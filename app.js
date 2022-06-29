cdd30ebf93fb7d1a45d5b763be987c9f6f3a7b36
gorgwallis <pianobirde@gmail.com>
2022-06-29 11:19:28 -0600

    updating app & index

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var app = new Vue({
     el: "#app",
-    data: {},
-    methods: {},
+    data: {
+        usernameInput: "",
+        passwordInput: "",
+
+        newEmailInput: "",
+        newPasswordInput: "",
+        newFullNameInput: "",
+    },
+    methods: {
+        getSession: async function () {
+            let response = await fetch(`$(URL)/session`,{
+                method: "GET",
+                credentials: "include"
+            });
+            console.log(response);
+        }
+        // POST /session - Attempt to log in
+        // POST /user - 
+    },
+    created: function () {
+        this.getSession();
+    }
 });
