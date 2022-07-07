2eab24c37d00793ad728177191b6404f4c8260b6
gorgwallis <pianobirde@gmail.com>
2022-07-07 10:18:49 -0600

    filling server, comments in index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//THIS right here is how big this file will ever be. It only needs to call the other parts of the app (the database, 
-//   the port, and the front-end).
+//THIS is how big this file will ever be. It only needs to call the other parts of the app (the database, 
+//   the port, and the front-end). This will be the file you call to run.
 
 const app = require("./server");
 //calls for express server
--- server.js
+++ server.js
@@ -2,14 +2,30 @@
 
 const express = require("express")
 //calls in express
-
 const app = express()
 //calls an instance of express
 
-app.use(express.json());
-//tells your app to use .json
+const cors = require("cors");
+const { process_params } = require("express/lib/router");
+//calls in cors - allows for middleware site authentication
+app.use(cors({
+    credentials: true,
+    origin: true
+}));
+// ^ This allows for...?
+app.use(express.json({}));
+//.json parser - tells your app to use .json
+
+const sessionSetUp = require("./session");
+sessionSetUp(app);
+// ^ This calls the session folder, which contains
+
+
+
 
 app.use(express.static(`${__dirname}/public/`));
 //tells the backend to serve the UI
 
 module.exports = app;
+//exports variables needed in other files. In this case, the variable for express.js
+
